0d60d7b173624f7d931a8659c3b240f13b448447
Yahckman <adeniranajibade2014@gmail.com>
2023-01-15 00:08:49 -0800

    second side

--- src/pages/homepage/book/book.jsx
+++ src/pages/homepage/book/book.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import FormInput from "../../../components/input/input";
 import styles from "./book.module.scss";
-import {Link} from 'react-router-dom'
-
+import { Link } from "react-router-dom";
+import {FaAccessibleIcon, FaTable} from 'react-icons/fa'
 function Book(props) {
   return (
     <div>
@@ -24,12 +24,27 @@ function Book(props) {
               <FormInput label="PICK-UP DATE" placeholder="Date" type="date" />
               <FormInput label="DROP-OFF DATE" placeholder="Date" type="date" />
               <FormInput label="PICK-UP TIME" placeholder="time" type="time" />
-              <Link  to='#' className={styles.btn}>Submit</Link >
+              <Link to="#" className={styles.btn}>
+                Submit
+              </Link>
             </form>
           </div>
 
-          
-          <div className="col-12 col-lg-8 col-md-8">2</div>
+          <div className="col-12 col-lg-8 col-md-8">
+            <div className={styles.car}>
+              <p className={styles.car_para1}>
+                Better Way to Rent a Perfect Car
+              </p>
+
+              <div className={styles.car_logo}>
+              <span className={`${styles.logos} ${styles.car_Logo1}`}> < FaAccessibleIcon/>  </span>
+              {/* <p>Choose ur pick-up location</p> */}
+              <span className={`${styles.logos} ${styles.car_Logo2}`}> < FaAccessibleIcon/>  </span>
+              <span className={`${styles.logos} ${styles.car_Logo3}`}> < FaAccessibleIcon/>  </span>
+
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
