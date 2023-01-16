e8a65fc4791eae160cff36b44cb54da5c9d750d2
Yahckman <adeniranajibade2014@gmail.com>
2023-01-16 06:51:28 -0800

    book finishing

--- src/pages/homepage/book/book.jsx
+++ src/pages/homepage/book/book.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import FormInput from "../../../components/input/input";
 import styles from "./book.module.scss";
 import { Link } from "react-router-dom";
-import {FaAccessibleIcon, FaTable} from 'react-icons/fa'
+import { FaRegHandshake, FaTable } from "react-icons/fa";
+import { RiUserLocationFill } from "react-icons/ri";
+import { AiOutlineCar } from "react-icons/ai";
 function Book(props) {
   return (
     <div>
@@ -37,11 +39,26 @@ function Book(props) {
               </p>
 
               <div className={styles.car_logo}>
-              <span className={`${styles.logos} ${styles.car_Logo1}`}> < FaAccessibleIcon/>  </span>
-              {/* <p>Choose ur pick-up location</p> */}
-              <span className={`${styles.logos} ${styles.car_Logo2}`}> < FaAccessibleIcon/>  </span>
-              <span className={`${styles.logos} ${styles.car_Logo3}`}> < FaAccessibleIcon/>  </span>
-
+                <div className="row">
+                  <div className={`col-12 col-lg-4 ${styles.logo}`}>
+                    <RiUserLocationFill className={`${styles.logos}`} />
+                    <p className={styles.logo_text}>
+                      Choose Your Pickup Location
+                    </p>
+                  </div>
+                  <div className={`col-12 col-lg-4 ${styles.logo}`}>
+                    <FaRegHandshake className={`${styles.logos}`} />
+                    <p className={styles.logo_text}>Select the Best Deal</p>
+                  </div>
+                  <div className={`col-12 col-lg-4 ${styles.logo}`}>
+                    <AiOutlineCar className={`${styles.logos}`} />
+                    <p className={styles.logo_text}>Reserve Your Rental Car</p>
+                  </div>
+                  <Link to="#">
+                    {" "}
+                    <p  className={styles.logo_btn}>Reserve Your car</p>
+                  </Link>
+                </div>
               </div>
             </div>
           </div>
