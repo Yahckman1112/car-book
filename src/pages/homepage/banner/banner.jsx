70eece09623943128cff1c1f532add4b7bff8f59
Yahckman <adeniranajibade2014@gmail.com>
2023-01-16 22:53:48 -0800

    setting stuffs

--- src/pages/homepage/banner/banner.jsx
+++ src/pages/homepage/banner/banner.jsx
@@ -6,10 +6,10 @@ function Banner(props) {
             
             <div className={styles.banner}>
                 <p className={styles.para1}>Fast & Easy Way To Rent A Car</p>
-                <p className={styles.para2}>
-                    <span className={styles.para2_main}>A small river named Duden flows by their place and supplies it with the</span>
-                    <span className={styles.para2_sub}>necessary regelialia. It is a paradisematic country, in which roasted parts</span>
-                </p>
+                <div className={styles.para2}>
+                    <p className={styles.para2_main}>A small river named Duden flows by their place and supplies it with the
+                   necessary regelialia. It is a paradisematic country, in which roasted parts</p>
+                </div>
             </div>
 
         </div>
