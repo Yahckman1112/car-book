cf98853d489b8e54d6101566680938f85e928106
Yahckman <adeniranajibade2014@gmail.com>
2023-02-01 02:32:39 -0800

    the service footer

--- src/pages/homepage/services/services.jsx
+++ src/pages/homepage/services/services.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { FaAccessibleIcon } from "react-icons/fa";
 import styles from "./services.module.scss";
 import { RiCarWashingLine, RiPoliceCarLine } from "react-icons/ri";
+import { Link } from "react-router-dom";
 import {
   MdOutlineDirectionsCar,
   MdOutlineDirectionsCarFilled,
@@ -53,6 +54,21 @@ function Services(props) {
               A small river named Duden flows by their place and supplies it
               with the necessary regelialia.
             </p>
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.service_footer}>
+        <div className={styles.footer_body}>
+          <div className={styles.footer_body_main}></div>
+          <div className={styles.footer_body_sub}>
+            <div className={styles.footer_content}>
+              <p className={styles.footer_content_para} >Do You Want To Earn With Us? So Don't Be Late.</p>
+
+              <Link to="#" className="btn btn-primary btn-lg">
+                Become a driver
+              </Link>
+            </div>
           </div>
         </div>
       </div>
