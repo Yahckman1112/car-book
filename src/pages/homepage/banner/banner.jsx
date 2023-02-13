e67da2e35a7d26a4a15e1e548094089d29e26564
Yahckman <adeniranajibade2014@gmail.com>
2023-02-13 07:14:40 -0800

    fixing banner

--- src/pages/homepage/banner/banner.jsx
+++ src/pages/homepage/banner/banner.jsx
@@ -1,19 +1,22 @@
-import React from 'react';
-import styles from './banner.module.scss'
+import React from "react";
+import styles from "./banner.module.scss";
 function Banner(props) {
-    return (
-        <div>
-            
-            <div className={styles.banner}>
-                <p className={styles.para1}>Fast & Easy Way To Rent A Car</p>
-                <div className={styles.para2}>
-                    <p className={styles.para2_main}>A small river named Duden flows by their place and supplies it with the
-                   necessary regelialia. It is a paradisematic country, in which roasted parts</p>
-                </div>
-            </div>
+  return (
+    <div>
+      <div className={styles.banner}>
+        <div className={styles.textContainer}>
 
+        <p className={styles.para1}>Fast & Easy Way To Rent A Car</p>
+
+        <p className={styles.para2}>
+          A small river named Duden flows by their place and supplies it with
+          the necessary regelialia. It is a paradisematic country, in which
+          roasted parts
+        </p>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default Banner;
