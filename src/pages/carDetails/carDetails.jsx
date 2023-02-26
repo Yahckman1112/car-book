f3dc249d697f45e78d351ad96b1d331072cfc767
Yahckman <adeniranajibade2014@gmail.com>
2023-02-26 06:02:00 -0800

    fetures

--- src/pages/carDetails/carDetails.jsx
+++ src/pages/carDetails/carDetails.jsx
@@ -1,38 +1,36 @@
 import React from "react";
 import { BsSpeedometer2 } from "react-icons/bs";
+import { GiCarSeat, GiLightningSpanner } from "react-icons/gi";
+import { RiGasStationFill, RiLuggageCartLine } from "react-icons/ri";
 import BannerUsed from "./../../components/banner/banner";
 import styles from "./carDetails.module.scss";
 function CarDetails(props) {
   const carInfos = [
     {
       logo: <BsSpeedometer2 size={40} />,
-      text: 'Millage',
-      sub:'40,000'
+      text: "Millage",
+      sub: "40,000",
     },
     {
-      logo: <BsSpeedometer2 size={40} />,
-      text: 'Transmission',
-      sub:'Manual'
+      logo: <GiLightningSpanner size={40} />,
+      text: "Transmission",
+      sub: "Manual",
     },
     {
-      logo: <BsSpeedometer2 size={40} />,
-      text: 'Seats',
-      sub:'5 '
+      logo: < GiCarSeat size={40} />,
+      text: "Seats",
+      sub: "5  Adults",
     },
     {
-      logo: <BsSpeedometer2 size={40} />,
-      text: 'Luggage',
-      sub:'4 Bags'
+      logo: <RiLuggageCartLine size={40} />,
+      text: "Luggage",
+      sub: "4 Bags",
     },
+
     {
-      logo: <BsSpeedometer2 size={40} />,
-      text: 'Millage',
-      sub:'40,000'
-    },
-    {
-      logo: <BsSpeedometer2 size={40} />,
-      text: 'Fuel',
-      sub:'Petrol'
+      logo: <RiGasStationFill size={40} />,
+      text: "Fuel",
+      sub: "Petrol",
     },
   ];
   return (
@@ -47,27 +45,24 @@ function CarDetails(props) {
         <p className={styles.para1}>Chevrolette</p>
         <p className={styles.para2}>Mercedes Grand Sedan</p>
         <div className="container">
-
           <div className="row">
-            {carInfos.map((item,i)=>(
-            <div className="col-12 col-lg-2 col-md-3">
-              <div className={styles.carInfo}>
-                <p className={styles.info_logo}>
-                  {item.logo}
-                </p>
-                <p className={styles.info_detail}>
-                  <span className={styles.info_detail_main}>{item.text}</span>
-                  <span className={styles.info_detail_sub}>{item.sub}</span>
-                </p>
+            {carInfos.map((item, i) => (
+              <div className="col-12 col-lg-3 col-md-3">
+                <div className={styles.carInfo}>
+                  <p className={styles.info_logo}>{item.logo}</p>
+                  <p className={styles.info_detail}>
+                    <span className={styles.info_detail_main}>{item.text}</span>
+                    <span className={styles.info_detail_sub}>{item.sub}</span>
+                  </p>
+                </div>
               </div>
-            </div>
             ))}
-            
+
             {/* <div className="col-12 col-lg-2 col-md-3">2</div>
           <div className="col-12 col-lg-2 col-md-3">3</div>
           <div className="col-12 col-lg-2 col-md-3">4</div>
         <div className="col-12 col-lg-2 col-md-3">5</div> */}
-        </div>
+          </div>
         </div>
       </div>
     </div>
