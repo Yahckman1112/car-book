f1efa36ba3631a1922afcffa03d992301e0c743a
Yahckman <adeniranajibade2014@gmail.com>
2023-04-16 06:31:02 -0700

    admin auth1

--- src/pages/admin/AuthPage/AdminAuth.jsx
+++ src/pages/admin/AuthPage/AdminAuth.jsx
@@ -1,80 +1,91 @@
-import React from 'react';
-import FormInput from '../../../components/input/input';
+import React from "react";
+import FormInput from "../../../components/input/input";
 import { useFormik } from "formik";
 import * as Yup from "yup";
-import { Button } from 'reactstrap';
-import { auth } from '../../../firebas';
-import { useNavigate } from 'react-router-dom';
-import {signInWithEmailAndPassword } from 'firebase/auth'
+import { Button } from "reactstrap";
+import { auth } from "../../../firebas";
+import { useNavigate } from "react-router-dom";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { useState } from "react";
+import  Swal  from 'sweetalert2';
 
 function AdminAuth(props) {
-    
-    const navigate = useNavigate()
-    const MessageSubmit = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
+  const MessageSubmit = () => {
+    return Yup.object({
+      email: Yup.string().required("email is required").email(),
+      pwd: Yup.string().required("Suplly ur Password"),
+    });
+  };
 
-        return Yup.object({
-          email: Yup.string().required("email is required").email(),
-          pwd: Yup.string().required("Suplly ur Password"),
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      pwd: "",
+    },
+    validationSchema: MessageSubmit(),
+    onSubmit: async(values) => {
+      setIsLoading(true);
+
+     await signInWithEmailAndPassword(auth, values.email, values.pwd)
+        .then((userCredential) => {
+          const user = userCredential.user;
+          navigate("/");
+          console.log(user);
+        })
+        .catch((error) => {
+        setIsLoading(false)
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          console.log(errorCode, errorMessage);
+          Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text:error.code || 'Authentication fail',
+            showCancelButton:true,
+            showConfirmButton:false
+          })
         });
-      };
-    
-      const formik = useFormik({
-        initialValues: {
- 
-          email: "",
-          pwd: "",
-         
-        },
-        validationSchema: MessageSubmit(),
-        onSubmit: (values) => {
-            signInWithEmailAndPassword(auth, values.email, values.pwd)
-         .then((userCredential)=>{
-            const user=userCredential.user;
-            navigate('/')
-            console.log(user);
-            
-         })
+        setIsLoading(false)
+     
 
-         console.log(values);
-    
-         
-    
-          formik.handleReset();
-        },
-      });
-    return (
+      formik.handleReset();
+    },
+  });
+  return (
+    <div>
+      <form onSubmit={formik.handleSubmit}>
         <div>
-            <form onSubmit={formik.handleSubmit}>
-            <div>
-                <FormInput
-                  placeholder="Your name"
-                  name="email"
-                  value={formik.values.email}
-                  onChange={formik.handleChange}
-                  type='email'
-                />
-                {formik.touched.email && formik.errors.email && (
-                  <p className={''}>{formik.errors.email}</p>
-                )}
-              </div>
-            <div>
-                <FormInput
-                  placeholder="Your name"
-                  name="pwd"
-                  value={formik.values.pwd}
-                  onChange={formik.handleChange}
-                  type='password'
-                />
-                {formik.touched.pwd && formik.errors.pwd && (
-                  <p className={''}>{formik.errors.pwd}</p>
-                )}
-              </div>
-              <Button color="primary" type="submit">
-                 Login
-              </Button>
-            </form>
+          <FormInput
+            placeholder="Your name"
+            name="email"
+            value={formik.values.email}
+            onChange={formik.handleChange}
+            type="email"
+          />
+          {formik.touched.email && formik.errors.email && (
+            <p className={""}>{formik.errors.email}</p>
+          )}
         </div>
-    );
+        <div>
+          <FormInput
+            placeholder="Your name"
+            name="pwd"
+            value={formik.values.pwd}
+            onChange={formik.handleChange}
+            type="password"
+          />
+          {formik.touched.pwd && formik.errors.pwd && (
+            <p className={""}>{formik.errors.pwd}</p>
+          )}
+        </div>
+        <Button disabled={isLoading} color="primary" type="submit">
+          {isLoading ? 'Authenticating...': 'Login'}
+        </Button>
+      </form>
+    </div>
+  );
 }
 
 export default AdminAuth;
