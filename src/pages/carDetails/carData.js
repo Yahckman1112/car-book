d2101aec4687c165b3def6faa80cbe39ca26d972
Yahckman <adeniranajibade2014@gmail.com>
2023-05-02 04:56:00 -0700

    code refractoring

---
+++ src/pages/carDetails/carData.js
@@ -0,0 +1,110 @@
+import { BsSpeedometer2 } from "react-icons/bs";
+import { GiCarSeat, GiLightningSpanner } from "react-icons/gi";
+import { RiGasStationFill, RiLuggageCartLine } from "react-icons/ri";
+
+export const carInfos = [
+    {
+      logo: <BsSpeedometer2 size={40} />,
+      text: "Millage",
+      sub: "40,000",
+    },
+    {
+      logo: <GiLightningSpanner size={40} />,
+      text: "Transmission",
+      sub: "Manual",
+    },
+    {
+      logo: <GiCarSeat size={40} />,
+      text: "Seats",
+      sub: "5  Adults",
+    },
+    {
+      logo: <RiLuggageCartLine size={40} />,
+      text: "Luggage",
+      sub: "4 Bags",
+    },
+
+    {
+      logo: <RiGasStationFill size={40} />,
+      text: "Fuel",
+      sub: "Petrol",
+    },
+  ];
+
+  export const tabInfo = [
+    {
+      text: "Airconditions",
+      sign: "mark",
+      logo: <span>&#10003;</span>,
+    },
+    {
+      text: "  Child Seat",
+      sign: "mark",
+      logo: <span>&#10003;</span>,
+    },
+    {
+      text: "GPS",
+      sign: "wrong",
+      logo: <span>&#x2715;</span>,
+    },
+    {
+      text: "Luggage",
+      sign: "mark",
+      logo: <span>&#10003;</span>,
+    },
+    {
+      text: "Music",
+      sign: "mark",
+      logo: <span>&#10003;</span>,
+    },
+    {
+      text: "  Seat Belt",
+      sign: "mark",
+      logo: <span>&#10003;</span>,
+    },
+    {
+      text: "  Sleeping bed",
+      sign: "wrong",
+      logo: <span>&#x2715;</span>,
+    },
+    {
+      text: "  Water ",
+      sign: "mark",
+      logo: <span>&#10003;</span>,
+    },
+    {
+      text: "  Bluetooth ",
+      sign: "mark",
+      logo: <span>&#10003;</span>,
+    },
+    {
+      text: "  Onboard computer ",
+      sign: "wrong",
+      logo: <span>&#x2715;</span>,
+    },
+    {
+      text: "  Audio input ",
+      sign: "mark",
+      logo: <span>&#10003;</span>,
+    },
+    {
+      text: "  Long Term Trips ",
+      sign: "mark",
+      logo: <span>&#10003;</span>,
+    },
+    {
+      text: "  Car Kit ",
+      sign: "mark",
+      logo: <span>&#10003;</span>,
+    },
+    {
+      text: "  Climate control ",
+      sign: "mark",
+      logo: <span>&#10003;</span>,
+    },
+    {
+      text: "  Sleeping ",
+      sign: "mark",
+      logo: <span>&#10003;</span>,
+    },
+  ];
--- src/pages/carDetails/carDetails.jsx
+++ src/pages/carDetails/carDetails.jsx
@@ -1,7 +1,4 @@
-import React from "react";
-import { BsSpeedometer2 } from "react-icons/bs";
-import { GiCarSeat, GiLightningSpanner } from "react-icons/gi";
-import { RiGasStationFill, RiLuggageCartLine } from "react-icons/ri";
+import React, { useState, useEffect } from "react";
 import BannerUsed from "./../../components/banner/banner";
 import styles from "./carDetails.module.scss";
 import Tab from "react-bootstrap/Tab";
@@ -9,10 +6,10 @@ import Tabs from "react-bootstrap/Tabs";
 import Footer from "./../../components/footer/footer";
 import Header from "../../components/header/header";
 import { fireDB } from "../../firebas";
+import { doc, getDoc } from "firebase/firestore";
 import { useParams } from "react-router-dom";
-import { useState } from "react";
-import { useEffect } from "react";
-import { collection, doc, getDoc, getDocs } from "firebase/firestore";
+import { carInfos, tabInfo } from "./carData";
+
 function CarDetails(props) {
   const params = useParams();
   const id = params.id;
@@ -27,116 +24,7 @@ function CarDetails(props) {
     }
     getData();
   }, []);
-  const descriptionInfo = {
-    text: `      Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar. 
-    When she reached the first hills of the Italic Mountains, she had a last view back on the skyline of her hometown Bookmarksgrove, the headline of Alphabet Village and the  subline of her own road, the Line Lane. Pityful a rethoric question ran over her cheek, then she continued her way. `,
-  };
-  const carInfos = [
-    {
-      logo: <BsSpeedometer2 size={40} />,
-      text: "Millage",
-      sub: "40,000",
-    },
-    {
-      logo: <GiLightningSpanner size={40} />,
-      text: "Transmission",
-      sub: "Manual",
-    },
-    {
-      logo: <GiCarSeat size={40} />,
-      text: "Seats",
-      sub: "5  Adults",
-    },
-    {
-      logo: <RiLuggageCartLine size={40} />,
-      text: "Luggage",
-      sub: "4 Bags",
-    },
 
-    {
-      logo: <RiGasStationFill size={40} />,
-      text: "Fuel",
-      sub: "Petrol",
-    },
-  ];
-
-  const tabInfo = [
-    {
-      text: "Airconditions",
-      sign: "mark",
-      logo: <span>&#10003;</span>,
-    },
-    {
-      text: "  Child Seat",
-      sign: "mark",
-      logo: <span>&#10003;</span>,
-    },
-    {
-      text: "GPS",
-      sign: "wrong",
-      logo: <span>&#x2715;</span>,
-    },
-    {
-      text: "Luggage",
-      sign: "mark",
-      logo: <span>&#10003;</span>,
-    },
-    {
-      text: "Music",
-      sign: "mark",
-      logo: <span>&#10003;</span>,
-    },
-    {
-      text: "  Seat Belt",
-      sign: "mark",
-      logo: <span>&#10003;</span>,
-    },
-    {
-      text: "  Sleeping bed",
-      sign: "wrong",
-      logo: <span>&#x2715;</span>,
-    },
-    {
-      text: "  Water ",
-      sign: "mark",
-      logo: <span>&#10003;</span>,
-    },
-    {
-      text: "  Bluetooth ",
-      sign: "mark",
-      logo: <span>&#10003;</span>,
-    },
-    {
-      text: "  Onboard computer ",
-      sign: "wrong",
-      logo: <span>&#x2715;</span>,
-    },
-    {
-      text: "  Audio input ",
-      sign: "mark",
-      logo: <span>&#10003;</span>,
-    },
-    {
-      text: "  Long Term Trips ",
-      sign: "mark",
-      logo: <span>&#10003;</span>,
-    },
-    {
-      text: "  Car Kit ",
-      sign: "mark",
-      logo: <span>&#10003;</span>,
-    },
-    {
-      text: "  Climate control ",
-      sign: "mark",
-      logo: <span>&#10003;</span>,
-    },
-    {
-      text: "  Sleeping ",
-      sign: "mark",
-      logo: <span>&#10003;</span>,
-    },
-  ];
   return (
     <div className={styles.detailPage}>
       <Header />
@@ -197,7 +85,7 @@ function CarDetails(props) {
               className={styles.decription_tab}
               style={{ whiteSpace: "pre-line" }}
             >
-              {descriptionInfo.text}
+              {car.details}
             </div>
           </Tab>
           <Tab eventKey="review" title="Review">
