1be9c4f53c71342dd03b0e184d0b609ed25cc634
Yahckman <adeniranajibade2014@gmail.com>
2023-05-15 10:14:56 -0700

    fixing home p1

--- src/pages/homepage/feature/fetureCard.jsx
+++ src/pages/homepage/feature/fetureCard.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import styles from "./feature.module.scss";
 import img from "./../../../assets/car-4.jpg";
 import { Link } from "react-router-dom";
-import { CardBody, CardTitle, CardSubtitle, CardText, Card } from "reactstrap";
-function FetureCard({image, carName, days}) {
+import { CardBody, Card } from "reactstrap";
+function FetureCard({ image, carName, days }) {
   return (
     <div>
       <Card
@@ -15,18 +15,25 @@ function FetureCard({image, carName, days}) {
         <CardBody>
           <p className={styles.card_para1}>
             {" "}
-            <Link to="#">{carName}</Link>{" "}
+            <Link className={styles.carName} to="#">
+              {carName}
+            </Link>{" "}
           </p>
           <p>
             <span className={styles.card_para2}>Chevrolet</span>
             <span className={styles.card_para3}>
               {" "}
-              {days}<span className={styles.card_para3_sub}>/day</span>
+              {days}
+              <span className={styles.card_para3_sub}>/day</span>
             </span>
           </p>
           <div className={styles.card_btn}>
-          <Link to="#" className={styles.card_btn_book}>Book</Link>
-          <Link to="#" className={styles.card_btn_det}>Details</Link>
+            <Link to="#" className={styles.card_btn_book}>
+              Book
+            </Link>
+            <Link to="#" className={styles.card_btn_det}>
+              Details
+            </Link>
           </div>
         </CardBody>
       </Card>
