3a3d6abc15b952933bee212b3ef7af8deb7cd2cb
Yahckman <adeniranajibade2014@gmail.com>
2023-05-17 09:18:32 -0700

    break

--- src/pages/pricing/priceCard.jsx
+++ src/pages/pricing/priceCard.jsx
@@ -1,30 +1,51 @@
-import React from "react";
+import React, { useState,useEffect } from "react";
 import styles from "./pricing.module.scss";
 import { FcLikePlaceholder, FcLike } from "react-icons/fc";
 import { FaCog, FaCogs, FaSuitcase, FaUser } from "react-icons/fa";
 import { GiCarDoor, GiGearStick } from "react-icons/gi";
 import { TbManualGearbox } from "react-icons/tb";
 import Badge from "react-bootstrap/Badge";
-import { useState } from "react";
+
 import carImg from "./../../assets/car-1.jpg";
 import pay1 from "./../../assets/card_1.webp";
 import pay2 from "./../../assets/card_2.webp";
 import pay3 from "./../../assets/card_3.webp";
+import { collection, getDocs } from "firebase/firestore";
+import { fireDB } from "../../firebas";
 function PriceCard(props) {
   const [like, setLike] = useState(true);
+  const [cars, setCars] = useState([]);
+  const [carInfo, setCarIfo] = useState({})
+const carCollectionRef = collection(fireDB, 'carDetails')
 
-  const handleLike = () => {
-    setLike(!like);
-  };
+useEffect(()=>{
+    async function getData(){
+        const data = await getDocs(carCollectionRef);
+        console.log(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+        setCars(data .docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+        // console.log(data .docs.map((doc) => ({ ...doc.data().carInfo })));
+        setCarIfo(data .docs.map((doc) => ({ ...doc.data().carInfo })));
+        
+    }
+    getData()
+}, [])
   return (
     <div className="">
-      <div className={styles.card}>
+        {cars.map((item=>(
+            <div className={styles.card}>
+         
+         
         <div className={styles.card_top}>
-          <div style={{ cursor: "pointer" }} onClick={handleLike}>
+          <div
+            style={{ cursor: "pointer" }}
+            onClick={() => {
+              setLike(!like);
+            }}
+          >
             {like ? <FcLikePlaceholder size={20} /> : <FcLike size={20} />}
           </div>
           <div>
-            <p className={styles.card_para1}>Toyota Island</p>
+            <p className={styles.card_para1}>{item.carName}</p>
           </div>
           <p className={styles.card_para2}>
             {" "}
@@ -40,10 +61,9 @@ function PriceCard(props) {
         <div className="row">
           <div className="col-12 col-lg-4 col-md-4">
             <img
-              className='img-fluid'
+              className="img-fluid"
               src={carImg}
               height={170}
-            
               style={{ borderRadius: ".5rem" }}
               alt="car picture"
             />
@@ -51,11 +71,12 @@ function PriceCard(props) {
           <div className="col-12 col-lg-4 col-md-4">
             <div className={styles.card_car_info}>
               <div className={styles.card_car_info_option_1}>
-                <FaUser size={23} />Seat: 4
+                <FaUser size={23} />
+                Seat: {'1'}
               </div>
 
               <div className={styles.card_car_info_option_2}>
-                <FaSuitcase size={23} /> Baggage: 1 
+                <FaSuitcase size={23} /> Baggage: 1
               </div>
               <div className={styles.card_car_info_option_3}>
                 <GiCarDoor size={23} /> Doors: 2
@@ -75,17 +96,18 @@ function PriceCard(props) {
 
               <div className={styles.pay_price}>
                 <p className={styles.price1}>Price for 4days</p>
-                <p className={styles.price2}>#10,000</p>
-                <p className={styles.price3}>#8,500</p>
+                <p className={styles.price2}>#{item.priceS}</p>
+                <p className={styles.price3}>#{item.price}</p>
 
                 <a href="#" className={styles.btn_custom}>
-                    Rent a Car
-                  </a>
+                  Rent a Car
+                </a>
               </div>
             </div>
           </div>
         </div>
       </div>
+        )))}
     </div>
   );
 }
