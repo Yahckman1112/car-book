2c940efe4ebaf30a7b0a742fc6ea9a44dc22d38a
Yahckman <adeniranajibade2014@gmail.com>
2023-07-21 17:40:43 +0100

    validating message form

--- src/pages/blog/blog.jsx
+++ src/pages/blog/blog.jsx
@@ -1,22 +1,44 @@
 import React from "react";
 import Header from "../../components/header/header";
 import Banner from "../../components/banner/banner";
-import styles from './blog.module.scss'
+import styles from "./blog.module.scss";
+import img from '../../assets/image_1.jpg'
+import Coment from "./coment";
 
 function Blog(props) {
   return (
     <div>
       <Header />
-   <div>
-    < Banner pageNameSub={'Blog > sdsds'} PageName={' Read Our Blog'}/>
-   </div>
+      <div>
+        <Banner pageNameSub={"Blog > sdsds"} PageName={" Read Our Blog"} />
+      </div>
 
-   <div className={styles.blog}>
-    <div className="row">
-        <div className="col-12 col-lg-9 col-md-8"></div>
-        <div className="col-12 col-lg-3 col-md-4"></div>
-    </div>
-   </div>
+      <div className={styles.blog}>
+        <img src={img} className={`img-fluid ${styles.img}`} alt="" />
+        <div className="row">
+          <div className="col-12 col-lg-8 col-md-8">
+            <div className={styles.side_1}>
+              <p className={styles.para_1}>
+                It is a long established fact a reader be distracted
+              </p>
+              <p className={styles.para_2}>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Reiciendis, eius mollitia suscipit, quisquam doloremque
+                distinctio perferendis et doloribus unde architecto optio
+                laboriosam porro adipisci sapiente officiis nemo accusamus ad
+                praesentium? Esse minima nisi et. Dolore perferendis, enim
+                praesentium omnis, iste doloremque quia officia optio deserunt
+                molestiae voluptates soluta architecto tempora.
+              </p>
+
+              < Coment/>
+            </div>
+          </div>
+          <div className="col-12 col-lg-4 col-md-4">
+            <p style={{color:'#999999', fontSize:'2rem'}}> Advertize Here</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/pages/blog/coment.jsx
@@ -0,0 +1,80 @@
+import React from "react";
+import styles from "./blog.module.scss";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+
+function Coment(props) {
+  const validateComment = () => {
+    return Yup.object({
+      name: Yup.string().required("Enter ur name please"),
+      email: Yup.string().required("Enter ur name please").email(),
+      name: Yup.string().required("Tell us something"),
+    });
+  };
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      message: "",
+    },
+    validationSchema: validateComment(),
+
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
+console.log(formik);
+  return (
+    <div>
+      <h1>Leave a comment</h1>
+      <div className={`bg-light ${styles.container}`}>
+        <form  onSubmit={formik.handleSubmit}>
+          <div class="mb-3">
+            <label for="exampleFormControlInput1" class="form-label">
+              Name
+            </label>
+            <input 
+            name="name"
+                value={formik.values.name}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+            type="text" class="form-control" placeholder="John Doe" />
+          </div>
+          <div class="mb-3">
+            <label for="exampleFormControlInput1" class="form-label">
+              Email address
+            </label>
+            <input
+            name='email'
+                value={formik.values.email}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+              type="email"
+              class="form-control"
+              placeholder="name@example.com"
+            />
+          </div>
+          <div class="mb-3">
+            <label for="exampleFormControlTextarea1" class="form-label">
+              Message
+            </label>
+            <textarea
+            name="message"
+                value={formik.values.message}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+              class="form-control"
+              id="exampleFormControlTextarea1"
+              rows="3"
+            ></textarea>
+          </div>
+          <button type="submit" class="btn btn-primary">
+            Send Cooment
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Coment;
